714c256cc9bf66ef2460372d4defab87ecf1f127
Aveelash <aveelash.hota04@gmail.com>
2025-07-16 13:15:55 +0530

    Added delete functionality

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -5,6 +5,7 @@ import Loader from "../../components/Loader";
 import {
   useGetProductsQuery,
   useCreateProductMutation,
+  useDeleteProductMutation,
 } from "../../slices/productApiSlice.js";
 import { toast } from "react-toastify";
 import { Link } from "react-router-dom";
@@ -15,8 +16,19 @@ const ProductListScreen = () => {
   const [createProduct, { isLoading: loadingCreate }] =
     useCreateProductMutation();
 
-  const deleteHandler = (id) => {
-    console.log("delete", id);
+  const [deleteProduct, { isLoading: loadingDelete }] =
+    useDeleteProductMutation();
+
+  const deleteHandler = async (id) => {
+    if (window.confirm("Are you sure?")) {
+      try {
+        await deleteProduct(id);
+        refetch();
+        toast.success("Product Deleted!");
+      } catch (err) {
+        toast.error(err?.data?.message || err.error);
+      }
+    }
   };
 
   const createProductHandler = async () => {
@@ -44,6 +56,7 @@ const ProductListScreen = () => {
       </Row>
 
       {loadingCreate && <Loader />}
+      {loadingDelete && <Loader />}
 
       {isLoading ? (
         <Loader />
